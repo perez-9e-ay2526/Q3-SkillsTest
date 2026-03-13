72ac6a4e74c358fcfd10e437b659d2a07107a616
perez-9e-ay2526 <perez.za@obmontessori.edu.ph>
2026-03-13 11:34:38 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-
 function register(){
 
     const admin_Fee = 50.00;    
     const ticket_Price = 250.00;
-    let total_Num = Number(sub_Total + admin_Fee);
     let qty_Num = document.getElementById("quanNum").value;
-    let sub_Total = Number(qty_Num * ticket_Price)
+    let sub_Total = qty_Num * ticket_Price;
+    let total_Num = sub_Total + admin_Fee;
 
-    document.getElementById("final_Total").value=valArea.toFixed(2);
+    document.getElementById("final_subtotal").value=sub_Total.toFixed(2);
+    document.getElementById("final_Total").value=total_Num.toFixed(2);
 }
